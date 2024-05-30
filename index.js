e0318cc1f571474ec4b6d8efdfbac17b1dc9fbac
Serey Vatanak <45728734+vatanak10@users.noreply.github.com>
2024-05-30 11:08:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var os = require('os');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(`<h1>Hello. This is ${os.hostname()}!!!</h1>
-        <h1>OS Uptime: ${os.uptime()}</h1>
+        <h1>OS Uptime: ${os.uptime()}...</h1>
     `);
 }).listen(8080);
