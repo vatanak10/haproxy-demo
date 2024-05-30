1809b1ecdae385818d4f71a6f51255045ab1e2e7
Serey Vatanak <45728734+vatanak10@users.noreply.github.com>
2024-05-30 15:42:20 +0700

    Update index.js - add ip address

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var http = require('http');
 var os = require('os');
+
 http.createServer(function (req, res) {
+    var networkInterfaces = os.networkInterfaces();
+    var ip = networkInterfaces['eth0'][0].address;
+    
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(` <h1>This is OS Uptime: ${os.uptime()}...</h1>`);
+    res.end(`
+        <h1>OS Uptime: ${os.uptime()}</h1>
+        <h2>IP Address: ${ip}</h2>
+    `);
 }).listen(8080);
